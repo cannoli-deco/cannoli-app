9ec1fad51f144189b64694e87ac5790e712f38f3
RW21 <rinze.watanabe.translation@gmail.com>
2020-08-17 11:26:21 +1000

    Added navigation bar

---
+++ lib/navigation_bar.dart
@@ -0,0 +1,65 @@
+import 'package:flutter/material.dart';
+
+class NavigationBar extends StatefulWidget {
+  NavigationBar({Key key}) : super(key: key);
+
+  @override
+  _NavigationBarState createState() => _NavigationBarState();
+}
+
+class _NavigationBarState extends State<NavigationBar> {
+  int _selectedIndex = 0;
+  static const TextStyle optionStyle =
+  TextStyle(fontSize: 30, fontWeight: FontWeight.bold);
+
+
+  // Import and add your components here
+  static const List<Widget> _widgetOptions = <Widget>[
+    Text(
+        'Placeholder',
+        style: optionStyle
+    ),
+    Text(
+      'Placeholder',
+      style: optionStyle,
+    ),
+    Text(
+      'Placeholder',
+      style: optionStyle,
+    )
+  ];
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+          child: _widgetOptions.elementAt(_selectedIndex)
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+          items: const<BottomNavigationBarItem>[
+            BottomNavigationBarItem(
+                icon: Icon(Icons.home),
+                title: Text('Home')
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.pie_chart),
+              title: Text('Placeholder'),
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.pie_chart),
+              title: Text('Placeholder'),
+            ),
+          ],
+          currentIndex: _selectedIndex,
+          selectedItemColor: Colors.amber[800],
+          onTap: _onItemTapped
+      ),
+    );
+  }
+}
