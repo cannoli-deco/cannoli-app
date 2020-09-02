a41dfb7ccc472b2f3dc01165a25ec9219e2dcda3
pu-pu <abhigyan380@gmail.com>
2020-09-03 00:51:20 +1000

    Added Home Input Page. Added aditional widgets.dart file to store general widgets.

--- lib/routes.dart
+++ lib/routes.dart
@@ -1,11 +1,12 @@
 import 'package:cannoli_app/inputs/car_input.dart';
+import 'package:cannoli_app/inputs/home_input.dart';
 import 'package:flutter/material.dart';
 
 import 'inputs/input_list.dart';
 
-
-final routes = <String, WidgetBuilder> {
+final routes = <String, WidgetBuilder>{
   // we don't need home because it is specified in main
   '/input': (context) => InputList(),
   '/input/car': (context) => CarInput(),
+  '/input/electricity': (context) => HomeInput(),
 };
