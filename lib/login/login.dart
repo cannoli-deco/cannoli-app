4fa8b32858b0c8d3dda85e94cb9cb592b672a7e5
irene-33 <zferna@outlook.com>
2020-09-19 18:44:35 +1000

    re-done firebase connection

---
+++ lib/login/login.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+
+class LoginPage extends StatefulWidget {
+  @override
+  _LoginPageState createState() => new _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  String _email, _password;
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+
+  @override
+  Widget build
+}
