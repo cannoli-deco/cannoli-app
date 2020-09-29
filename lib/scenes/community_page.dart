97a647ba5b54e2e2b9fa97fdc8198b33803d956a
RW21 <rinze.watanabe.translation@gmail.com>
2020-09-29 10:23:51 +1000

    Initial implementation of article feed

--- lib/scenes/community_page.dart
+++ lib/scenes/community_page.dart
@@ -1,4 +1,6 @@
+import 'package:cannoli_app/articles.dart';
 import 'package:cannoli_app/community_card.dart';
+import 'package:cannoli_app/community_feed.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 
@@ -10,24 +12,27 @@ class CommunityPage extends StatefulWidget {
 class _CommunityPageState extends State<CommunityPage> {
   @override
   Widget build(BuildContext context) {
-    return
-        ListView  (
-          padding: EdgeInsets.all(8),
-            children: [StreamBuilder(
-              stream: FirebaseFirestore.instance.collection("posts")
-                  .snapshots(),
-              builder: (context, snapshot) {
-                if (snapshot.hasData) {
-                DocumentSnapshot item = snapshot.data.documents[0];
-                return CommunityCard(
-                userName: item.data()["owner_name"],
-                content: item.data()["content"],
-                );
-                } else {
-                return Text("none");
-                }
-              },
-            )]
-        );
+    return DefaultTabController(
+        length: 2,
+        child: Scaffold(
+          appBar: PreferredSize(
+              preferredSize: Size.fromHeight(48) ,//Size.fromHeight(kToolbarHeight),
+              child: Container(
+                  color: Theme.of(context).primaryColor,
+                  child: SafeArea(
+                      child: Column(
+                    children: [
+                      TabBar(
+                        tabs: [
+                          Tab(icon: Icon(Icons.people)),
+                          Tab(icon: Icon(Icons.book)),
+                        ],
+                      )
+                    ],
+                  )))),
+          body: TabBarView(
+            children: [CommunityFeed(), Articles()],
+          ),
+        ));
   }
 }
