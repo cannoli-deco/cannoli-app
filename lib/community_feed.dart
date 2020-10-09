d7eaf471780be98a29ecff6a6348bdaa751de763
RW21 <rinze.watanabe.translation@gmail.com>
2020-10-09 10:33:10 +1000

    Add fab to community page

--- lib/community_feed.dart
+++ lib/community_feed.dart
@@ -6,21 +6,33 @@ import 'community_card.dart';
 class CommunityFeed extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return ListView(padding: EdgeInsets.all(8), children: [
-      StreamBuilder(
-        stream: FirebaseFirestore.instance.collection("posts").snapshots(),
-        builder: (context, snapshot) {
-          if (snapshot.hasData) {
-            DocumentSnapshot item = snapshot.data.documents[0];
-            return CommunityCard(
-              userName: item.data()["owner_name"],
-              content: item.data()["content"],
-            );
-          } else {
-            return Text("none");
-          }
-        },
-      )
-    ]);
+    return Scaffold(
+        body: ListView(
+          padding: EdgeInsets.all(8),
+          children: [
+            StreamBuilder(
+              stream:
+                  FirebaseFirestore.instance.collection("posts").snapshots(),
+              builder: (context, snapshot) {
+                if (snapshot.hasData) {
+                  DocumentSnapshot item = snapshot.data.documents[0];
+                  return CommunityCard(
+                    userName: item.data()["owner_name"],
+                    content: item.data()["content"],
+                  );
+                } else {
+                  return Text("none");
+                }
+              },
+            )
+          ],
+        ),
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {},
+          child: Icon(Icons.add_comment),
+          backgroundColor: Theme.of(context).primaryColor,
+          shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.all(Radius.circular(16.0))),
+        ));
   }
 }
