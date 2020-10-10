cfafbcad378af16310737dcab8e16a93cec7cf04
irene-33 <zferna@outlook.com>
2020-10-11 05:27:56 +1000

    User profile template created
    Dummy values

--- lib/login/user_profile.dart
+++ lib/login/user_profile.dart
@@ -14,19 +14,62 @@ class _UserProfileState extends State<UserProfile> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        children: <Widget>[
-          Text('User Profile'),
-          Row(children: [
-            Text('Display name: '),
-            //Text(_auth.userProfile.displayName),
-          ]),
-          Row(children: [
-            Text('Email: '),
-            //Text(_auth.userProfile.email),
-          ]),
-        ],
-      ),
+      body: Column(children: [
+        Padding(
+            padding: EdgeInsets.all(20.0),
+            child: Column(
+              children: <Widget>[
+                Row(
+                  children: [
+                    Text(
+                      'Profile',
+                      style:
+                          TextStyle(fontWeight: FontWeight.bold, fontSize: 25),
+                    )
+                  ],
+                ),
+                SizedBox(height: 20),
+                Row(mainAxisAlignment: MainAxisAlignment.start, children: [
+                  Icon(
+                    Icons.person,
+                    size: 80.0,
+                  ),
+                  Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          'Display name: Eco-123',
+                          style: TextStyle(fontSize: 15),
+                        ),
+                        OutlineButton(child: Text('Change display name'))
+                        //Text(_auth.userProfile.displayName),
+                      ])
+                ]),
+                SizedBox(height: 15),
+                Row(children: [
+                  Text(
+                    'Email: eco@gmail.com',
+                    style: TextStyle(fontSize: 15),
+                  ),
+                  //Text(_auth.userProfile.email),
+                ]),
+                SizedBox(height: 15),
+                Row(
+                  children: <Widget>[
+                    Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            'Password: ******',
+                            style: TextStyle(fontSize: 15),
+                          ),
+                          OutlineButton(child: Text('Change password')),
+                        ]),
+                  ],
+                ),
+              ],
+            ))
+      ]),
       floatingActionButton: FloatingActionButton.extended(
         onPressed: () => _auth.signOut(),
         label: Text('Sign out'),
