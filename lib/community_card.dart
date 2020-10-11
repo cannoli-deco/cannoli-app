b6fb39c010f85e2b9ba432db22f4e260d618e553
Unknown <ivan_leong_97@hotmail.com>
2020-10-11 15:04:36 +0800

    Added widget template

--- lib/community_card.dart
+++ lib/community_card.dart
@@ -1,7 +1,8 @@
+import 'package:cannoli_app/widgets/feed_widget.dart';
 import 'package:flutter/material.dart';
 
 class CommunityCard extends StatefulWidget {
-  CommunityCard({Key key, this.content, this.userName}) :super(key: key);
+  CommunityCard({Key key, this.content, this.userName}) : super(key: key);
   final String content;
   final String userName;
 
@@ -10,67 +11,19 @@ class CommunityCard extends StatefulWidget {
 }
 
 class _CommunityCardState extends State<CommunityCard> {
-
   @override
   Widget build(BuildContext context) {
-    return
-      SizedBox(
-          width: 200,
-          height: 300,
-          child:
-          Scaffold(
-            body: Container(
-        padding: EdgeInsets.fromLTRB(10,10,10,0),
-              child: Card(
-                elevation: 5,
-                child: Padding(
-                  padding: EdgeInsets.all(7),
-                  child: Stack(
-                      children: <Widget>[
-                        Align(
-                          alignment: Alignment.centerRight,
-                          child: Stack(
-                            children: <Widget>[
-                              Padding(
-                                  padding: const EdgeInsets.only(left: 10, top: 5),
-                                  child: Column(
-                                    children: <Widget>[
-                                      Row(
-                                        children: <Widget>[
-                                          Padding(
-                                            padding: const EdgeInsets.only(left: 15.0),
-                                            child: Align(
-                                                alignment: Alignment.centerLeft,
-                                                child: Icon(
-                                                  Icons.person,
-                                                  color: Colors.amber,
-                                                  size: 40,
-                                                )),
-                                          ),
-                                         Text(
-                                           widget.userName
-                                         )
-                                        ],
-                                      ),
-                                      Divider(),
-                                      Row(
-                                        children: [
-                                          Text(
-                                            widget.content
-                                          )
-                                        ],
-                                      ),
-                                    ],
-                                  ))
-                            ],
-                          ),
-                        ),
-
-                      ]),
-                ),
-              ),
-            ),
-          )
-      );
+    int postLength = posts.length;
+    return SizedBox(
+      width: 300,
+      height: MediaQuery.of(context).size.height,
+        child: postLength >= 1 ? 
+        new ListView.builder (
+          itemCount: posts.length,
+          itemBuilder: (context, index) {
+          return posts[index];
+        },) 
+        : Center( child: RaisedButton( child: Text("Nothing to show..."), onPressed: () => addFeedPost(),))
+    );
   }
 }
