25cbdb876fbc7fdc9ec7bb55b90d1e4f4d1fba15
Mendes <kirikirimendes@gmail.com>
2020-10-16 16:43:28 +1000

    Changing the summary pie chart to actual data

--- lib/widgets/graph_view_widgets.dart
+++ lib/widgets/graph_view_widgets.dart
@@ -3,6 +3,8 @@ import 'package:cannoli_app/comparison_graph.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_circular_chart/flutter_circular_chart.dart';
 import 'package:flutter_sparkline/flutter_sparkline.dart';
+import 'package:cannoli_app/database.dart';
+import 'package:fl_chart/fl_chart.dart';
 
 final Color subTextColor = Colors.grey[600];
 // enum: For every graph widget in All/General Category
@@ -25,6 +27,52 @@ class _AllChartsState extends State<AllCharts> {
     AllGraphWidgetList.graphCategoryEmissions
   ];
   AllGraphWidgetList _currentWidget = AllGraphWidgetList.graphUserEmissions;
+
+  // List to record each type of comsumption
+  List<double> _consumptions;
+  List<String> sources = ['Transport', 'Home\n Energy'];
+  List<Color> sourceColors = [
+    CustomMaterialColor.buttonColorBlue[200],
+    CustomMaterialColor.subColorGrass[200]
+  ];
+
+  int touchedIndex;
+
+  void _loadEntries() async{
+    double totalConsumption = 0;
+    double transportConsumption = 0;
+    double homeConsumption = 0;
+    List<double> temp = [];
+
+    DateTime today = DateTime.now();
+    var allEntries = await entryFromDate(today);
+    if (allEntries.length != 0) {
+      for (int i = 0; i < allEntries.length; i++) {
+        print(allEntries[i].source_id);
+        if (allEntries[i].source_id == 1) {
+          homeConsumption += allEntries[i].consumption;
+        } else if (allEntries[i].source_id == 383) {
+          transportConsumption += allEntries[i].consumption;
+        }
+        totalConsumption += allEntries[i].consumption;
+      }
+    }
+
+    if (totalConsumption == 0) {
+      temp.add(transportConsumption / 1);
+      temp.add(homeConsumption / 1);
+    } else {
+      temp.add(transportConsumption / totalConsumption);
+      temp.add(homeConsumption / totalConsumption);
+    }
+
+    print("Detail Now:");
+    print(temp);
+
+    setState(() {
+      _consumptions = temp;
+    });
+  }
 
   // Function to switch between widget category groups Graph View
   Widget getWidgetGroup() {
@@ -174,12 +222,40 @@ class _AllChartsState extends State<AllCharts> {
                 ),
               ),
               Container(
-                height: 312.0,
-                padding: EdgeInsets.fromLTRB(10.0, 10.0, 10.0, 10.0),
-                child: AnimatedCircularChart(
-                  size: const Size(260, 260),
-                  initialChartData: circularData,
-                  chartType: CircularChartType.Pie,
+                /**Abhi's previous implementation**/
+                // height: 312.0,
+                // padding: EdgeInsets.fromLTRB(10.0, 10.0, 10.0, 10.0),
+                // child: AnimatedCircularChart(
+                //   size: const Size(260, 260),
+                //   initialChartData: circularData,
+                //   chartType: CircularChartType.Pie,
+                // ),
+                /**From here by Mendes**/
+                child: Expanded(
+                  child: AspectRatio(
+                    aspectRatio: 1,
+                    child: PieChart(
+                      PieChartData(
+                          pieTouchData:
+                          PieTouchData(touchCallback: (pieTouchResponse) {
+                            setState(() {
+                              if (pieTouchResponse.touchInput is FlLongPressEnd ||
+                                  pieTouchResponse.touchInput is FlPanEnd) {
+                                touchedIndex = -1;
+                              } else {
+                                touchedIndex = pieTouchResponse.touchedSectionIndex;
+                              }
+                            });
+                          }),
+                          startDegreeOffset: 180,
+                          borderData: FlBorderData(
+                            show: false,
+                          ),
+                          sectionsSpace: 12,
+                          centerSpaceRadius: 0,
+                          sections: showingSections()),
+                    ),
+                  ),
                 ),
               ),
             ],
@@ -187,6 +263,104 @@ class _AllChartsState extends State<AllCharts> {
         ),
       ],
     );
+  }
+
+  List<PieChartSectionData> noSection() {
+    return List.generate(1, (i) {
+      final isTouched = i == touchedIndex;
+      final double fontSize = isTouched ? 34 : 30;
+      //final double radius = isTouched ? 115 : 115;
+      final double opacity = isTouched ? 1 : 0.6;
+      switch (i) {
+        case 0:
+          return PieChartSectionData(
+            titlePositionPercentageOffset: 0,
+            color: CustomMaterialColor.buttonColorWhite,
+            value: 1,
+            title: "No emission",
+            radius: 80,
+            titleStyle: TextStyle(
+                fontSize: fontSize,
+                fontWeight: FontWeight.bold,
+                color: CustomMaterialColor.emphasisColor),
+          );
+        default:
+          return null;
+      }
+    });
+  }
+
+  List<PieChartSectionData> oneSection(int index) {
+    return List.generate(1, (i) {
+      final isTouched = i == touchedIndex;
+      final double fontSize = isTouched ? 28 : 18;
+      //final double radius = isTouched ? 115 : 95;
+      final double opacity = isTouched ? 1 : 0.6;
+      switch (i) {
+        case 0:
+          return PieChartSectionData(
+            color: sourceColors[index],
+            value: _consumptions[index],
+            title: sources[index],
+            radius: 80,
+            titleStyle: TextStyle(
+                fontSize: fontSize,
+                fontWeight: FontWeight.bold,
+                color: const Color(0xffffffff)),
+          );
+        default:
+          return null;
+      }
+    });
+  }
+
+  List<PieChartSectionData> allSections() {
+    return List.generate(2, (i){
+      final isTouched = i == touchedIndex;
+      final double opacity = isTouched ? 1 : 0.6;
+
+      switch (i){
+        case 0:
+          return PieChartSectionData(
+            color: sourceColors[0].withOpacity(opacity),
+            value: _consumptions[0],
+            title: sources[0],
+            radius: 80,
+            titleStyle: TextStyle(
+                fontSize: 18, fontWeight: FontWeight.bold, color: const Color(0xff044d7c)),
+            titlePositionPercentageOffset: 0.55,
+          );
+        case 1:
+          return PieChartSectionData(
+            color: sourceColors[0].withOpacity(opacity),
+            value: _consumptions[0],
+            title: sources[0],
+            radius: 65,
+            titleStyle: TextStyle(
+                fontSize: 18, fontWeight: FontWeight.bold, color: const Color(0xff90672d)),
+            titlePositionPercentageOffset: 0.55,
+          );
+        default:
+          return null;
+      }
+    });
+  }
+
+  List<PieChartSectionData> showingSections() {
+    if (_consumptions[0] != 0.0 &&
+        _consumptions[1] != 0.0) {
+      return allSections();
+    }
+
+    if (_consumptions[0] != 0.0){
+      return oneSection(0);
+    }else if(_consumptions[1] != 0.0){
+      return oneSection(1);
+    }else{
+      return noSection();
+    }
+
+
   }
 
   @override
